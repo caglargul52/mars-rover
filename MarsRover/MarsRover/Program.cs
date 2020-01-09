d57f5c2607335096f6f3ce2535b0227e0341160f
Çağlar GÜL <caglar.g52@hotmail.com>
2020-01-09 17:30:09 +0300

    Rover classı oluştruldu.

--- MarsRover/MarsRover/Program.cs
+++ MarsRover/MarsRover/Program.cs
@@ -10,6 +10,17 @@ namespace MarsRover
     {
         static void Main(string[] args)
         {
+            Rover rover = new Rover(5, 5);
+
+            rover.SetInitialCoordinate(new Location { XPoint = 1, YPoint = 2, Direction = Directions.N });
+            rover.Move("LMLMLMLMM");
+            Console.WriteLine("X Point: " + rover.Location.XPoint + "  Y Point: " + rover.Location.YPoint + "  Direction: " + rover.Location.Direction);
+
+            rover.SetInitialCoordinate(new Location { XPoint = 3, YPoint = 3, Direction = Directions.E });
+            rover.Move("MMRMMRMRRM");
+            Console.WriteLine("X Point: " + rover.Location.XPoint + "  Y Point: " + rover.Location.YPoint + "  Direction: " + rover.Location.Direction);
+
+            Console.Read();
         }
     }
 }
---
+++ MarsRover/MarsRover/Rover.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    class Rover
+    {
+        public Location Location { get; set; }
+        private int _coordinatLimitX;
+        private int _coordinatLimitY;
+
+        public Rover(int coordinatLimitX, int coordinatLimitY)
+        {
+            _coordinatLimitX = coordinatLimitX;
+            _coordinatLimitY = coordinatLimitY;
+        }
+
+        public void SetInitialCoordinate(Location location)
+        {
+            Location = location;
+
+            if (location.XPoint > _coordinatLimitX || location.YPoint > _coordinatLimitY)
+            {
+                Location.XPoint = null;
+                Location.YPoint = null;
+
+                throw new Exception("Error Limit Exceeded");
+            }
+        }
+
+        private void RotateLeft()
+        {
+            switch (Location.Direction)
+            {
+                case Directions.E:
+                    Location.Direction = Directions.N;
+                    break;
+                case Directions.W:
+                    Location.Direction = Directions.S;
+                    break;
+                case Directions.N:
+                    Location.Direction = Directions.W;
+                    break;
+                case Directions.S:
+                    Location.Direction = Directions.E;
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        private void RotateRight()
+        {
+            switch (Location.Direction)
+            {
+                case Directions.E:
+                    Location.Direction = Directions.S;
+                    break;
+                case Directions.W:
+                    Location.Direction = Directions.N;
+                    break;
+                case Directions.N:
+                    Location.Direction = Directions.E;
+                    break;
+                case Directions.S:
+                    Location.Direction = Directions.W;
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        private void StepForward()
+        {
+            switch (Location.Direction)
+            {
+                case Directions.E:
+                    Location.XPoint++;
+                    break;
+                case Directions.W:
+                    Location.XPoint--;
+                    break;
+                case Directions.N:
+                    Location.YPoint++;
+                    break;
+                case Directions.S:
+                    Location.YPoint--;
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+
+            if (Location.XPoint > _coordinatLimitX || Location.YPoint > _coordinatLimitY)
+            {
+                Location.XPoint = null;
+                Location.YPoint = null;
+
+                throw new Exception("Error Limit Exceeded");
+            }
+        }
+
+        public void Move(string command)
+        {
+            char[] commandArray = command.ToCharArray();
+
+            if (Location.XPoint != null && Location.YPoint != null)
+            {
+                foreach (var item in commandArray)
+                {
+                    switch (item)
+                    {
+                        case 'L':
+                            RotateLeft();
+                            break;
+                        case 'R':
+                            RotateRight();
+                            break;
+                        case 'M':
+                            try
+                            {
+                                StepForward();
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                                return;
+                            }
+                            break;
+                        default:
+                            throw new ArgumentException();
+                    }
+                }
+            }
+
+            else
+            {
+                throw new Exception("The start coordinates are entered incorrectly!");
+            }
+        }
+    }
+}
